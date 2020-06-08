3c7e37bbdb093b7e3353b1a1d11283793c5b2da3
Alex Huntley <huntley.alexander@gmail.com>
2020-06-08 23:07:39 +0100

    Add absolute value element

--- formula.py
+++ formula.py
@@ -146,6 +146,10 @@ class Editor(Gtk.DrawingArea):
             self.cursor.greedy_insert(Expt)
             self.queue_draw()
             return
+        if char == "|":
+            self.cursor.insert(Abs(None))
+            self.queue_draw()
+            return
         try:
             direction = Direction(event.keyval)
             select = bool(event.state & Gdk.ModifierType.SHIFT_MASK)
@@ -913,6 +917,38 @@ class Radical(Element):
         ctx.stroke()
         ctx.move_to(0,0)
         self.radicand.draw(ctx, cursor, widget_transform)
+
+class Abs(Element):
+    def __init__(self, argument, parent=None):
+        super().__init__(parent)
+        self.argument = ElementList(argument, self)
+        self.lists = [self.argument]
+
+    def compute_metrics(self, ctx, metric_ctx):
+        self.argument.compute_metrics(ctx, metric_ctx)
+        self.bar = Text("|", ctx)
+        self.width = self.argument.width + 2*self.bar.width
+        self.ascent = self.argument.ascent
+        self.descent = self.argument.descent
+        super().compute_metrics(ctx, metric_ctx)
+
+    def draw_bar(self, ctx):
+        bar_height = self.bar.ink_rect.height
+        scale_factor = max(1, (self.ascent + self.descent)/bar_height)
+        with saved(ctx):
+            ctx.translate(0, -self.ascent)
+            ctx.scale(1, scale_factor)
+            ctx.translate(0, -self.bar.ink_rect.y)
+            ctx.move_to(0, 0)
+            self.bar.draw(ctx)
+
+    def draw(self, ctx, cursor, widget_transform):
+        super().draw(ctx, cursor, widget_transform)
+        self.draw_bar(ctx)
+        ctx.translate(self.bar.width, 0)
+        self.argument.draw(ctx, cursor, widget_transform)
+        ctx.translate(self.argument.width, 0)
+        self.draw_bar(ctx)
 
 class Paren(Element):
     h_spacing = 0
