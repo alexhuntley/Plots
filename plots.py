a9d4d6a723fe95c09795816747dc7b14ed7e0647
Alex Huntley <huntley.alexander@gmail.com>
2020-12-21 11:49:44 +0000

    Implemented OpenGL shader, zooming and panning

--- plots.py
+++ plots.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
@@ -8,8 +7,16 @@ import formula
 import converters
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from OpenGL.arrays import vbo
+from OpenGL.GL import shaders
+
+import numpy as np
 
 class Plots:
+    def __init__(self):
+        self.scale = 1
+        self.translation = np.array([0, 0], 'f')
+
     def on_destroy(self, *args):
         Gtk.main_quit()
 
@@ -26,6 +33,7 @@ class Plots:
         self.formula_box = builder.get_object("equation_box")
         self.gl_area = builder.get_object("gl")
         self.gl_area.connect("render", self.gl_render)
+        self.gl_area.connect("realize", self.gl_realize)
 
         for c in self.formula_box.get_children():
             self.formula_box.remove(c)
@@ -36,18 +44,94 @@ class Plots:
         self.window.set_default_size(600,400)
         self.window.show_all()
 
+        self.drag = Gtk.GestureDrag(widget=self.gl_area)
+        self.drag.set_propagation_phase(Gtk.PropagationPhase.CAPTURE)
+        self.drag.connect("drag-update", self.drag_update)
+        self.drag.connect("drag-begin", self.drag_begin)
+        self.gl_area.add_events(Gdk.EventMask.SMOOTH_SCROLL_MASK)
+        self.gl_area.connect('scroll_event', self.scroll_zoom)
+
         Gtk.main()
 
     def gl_render(self, area, context):
         area.make_current()
         w = area.get_allocated_width()
         h = area.get_allocated_height()
+        self.viewport = np.array([w, h], 'f')
         glViewport(0, 0, w, h)
 
-        glClearColor(1, 0, 1, 0)
+        glClearColor(0, 0, 1, 0)
         glClear(GL_COLOR_BUFFER_BIT)
 
+        shaders.glUseProgram(self.shader)
+        glUniform2f(glGetUniformLocation(self.shader, "viewport"), *self.viewport)
+        glUniform2f(glGetUniformLocation(self.shader, "translation"), *self.translation)
+        glUniform1f(glGetUniformLocation(self.shader, "scale"), self.scale)
+        glBindVertexArray(self.vao)
+        glDrawArrays(GL_TRIANGLES, 0, 18)
+        glBindVertexArray(0)
+
         return True
+
+    def gl_realize(self, area):
+        area.make_current()
+
+        if (area.get_error() != None):
+            return
+
+        VERTEX_SHADER = shaders.compileShader("""#version 330 core
+        layout (location = 0) in vec3 position;
+        out vec2 graph_pos;
+        uniform vec2 viewport;
+        uniform vec2 translation;
+        uniform float scale;
+        void main() {
+        gl_Position = vec4(position, 1.0);
+        vec2 normalised = position.xy * viewport / viewport.x;
+        graph_pos = normalised/scale - translation;
+        }""", GL_VERTEX_SHADER)
+
+        FRAGMENT_SHADER = shaders.compileShader("""#version 330 core
+        out vec4 color;
+        in vec2 graph_pos;
+        void main() {
+            color = vec4(abs(cos(graph_pos)), 1, 1 );
+        }""", GL_FRAGMENT_SHADER)
+
+        self.shader = shaders.compileProgram(VERTEX_SHADER, FRAGMENT_SHADER)
+        self.vbo = vbo.VBO(np.array([
+            [-1, -1, 0],
+            [-1, 1, 0],
+            [1, 1, 0],
+            [-1, -1, 0],
+            [1, -1, 0],
+            [1, 1, 0]
+        ],'f'), usage="GL_STATIC_DRAW_ARB")
+
+        self.vao = glGenVertexArrays(1)
+        glBindVertexArray(self.vao)
+        self.vbo.bind()
+        #self.vbo.copy_data()
+        glBufferData(GL_ARRAY_BUFFER, self.vbo.data.nbytes, self.vbo.data,  GL_STATIC_DRAW)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*self.vbo.data.itemsize, self.vbo)
+        glEnableVertexAttribArray(0)
+        self.vbo.unbind()
+        glBindVertexArray(0)
+
+    def drag_update(self, gesture, dx, dy):
+        dr = 2*np.array([dx, -dy], 'f')/self.viewport
+        self.translation = self.init_translation + dr/self.scale
+        print(dx, dy)
+        self.gl_area.queue_draw()
+
+    def drag_begin(self, gesture, start_x, start_y):
+        self.init_translation = self.translation
+
+    def scroll_zoom(self, widget, event):
+        _, dx, dy = event.get_scroll_deltas()
+        self.scale *= 1-dy/3
+        widget.queue_draw()
+
 
 if __name__ == '__main__':
     Plots().main()
