676d7865a8ae27576f8c5293f593d1087e1b78ef
Alex Huntley <huntley.alexander@gmail.com>
2021-01-19 15:09:11 +0000

    Update AppStream metainfo from bump_version.sh

--- bin/bump_version.sh
+++ bin/bump_version.sh
@@ -6,3 +6,11 @@ fi
 sed -i "s/version = '.*'/version = '$1'/" setup.py
 sed -i "s:<property name=\"version\">0.3.0</property>:<property name=\"version\">$1</property>:" plots/ui/about.glade
 dch --newversion $1 --distribution focal --maintmaint v$1
+sed -i res/com.github.alexhuntley.Plots.metainfo.xml -f - <<EOF
+s|<releases>|<releases>\n\
+    <release version="$1" date="$(date +%F)">\n\
+      <description>\n\
+        <p>$2</p>\n\
+      </description>\n\
+    </release>|
+EOF
