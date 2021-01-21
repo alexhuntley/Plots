649dbbdb2da6dc332df767238afe7e8869247d15
Alex Huntley <huntley.alexander@gmail.com>
2021-01-21 21:52:25 +0000

    add gpl notice to test_cursor.py

--- tests/test_cursor.py
+++ tests/test_cursor.py
@@ -1,3 +1,20 @@
+# Copyright 2021 Alexander Huntley
+
+# This file is part of Plots.
+
+# Plots is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Plots is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Plots.  If not, see <https://www.gnu.org/licenses/>.
+
 from plots.cursor import Cursor
 import plots.formula as f
 from plots.parser import from_latex
