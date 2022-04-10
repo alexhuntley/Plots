629195d806eb6ae38f2ce2ad4f6c3fa6a8d2be0e
Alex Huntley <huntley.alexander@gmail.com>
2022-04-10 23:49:13 +0200

    close popover on color selection

--- plots/colorpicker.py
+++ plots/colorpicker.py
@@ -72,6 +72,7 @@ class PopoverColorPicker(Gtk.Button):
 
     def on_color_activated(self, chooser, color):
         self.emit("color-activated", chooser, color)
+        self.popover.popdown()
         self.update_color()
 
     def on_button(self, widget, event):
