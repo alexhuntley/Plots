184626b14d09bbbda40c158f6b6a5976c6fb201c
benjamin <benjamin.soelch@googlemail.com>
2022-09-25 12:36:17 +0200

    allow "^" without braces
    hande "^" and "_" without braces ("{}") the same way they are handled in LaTeX (the next atom is super-/subscript)

--- plots/parser.py
+++ plots/parser.py
@@ -108,8 +108,8 @@ OPNAME : LETTER+
 operator : "\\operatorname{" OPNAME "}"
 
 subscriptsuperscript.2 : "_" blist "^" blist
-supersub : "_" blist -> subscript
-         | "^" blist -> superscript
+supersub : "_" (blist|atom) -> subscript
+         | "^" (blist|atom) -> superscript
 
 frac.10 : "\\frac" blist blist
 
